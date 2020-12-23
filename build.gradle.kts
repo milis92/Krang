57d9eebf282cb47a0ff6eab3a8a55fcdd31bc8cb
Ivan Milisavljevic <cartman.dev@gmail.com>
2020-12-23 16:37:23 +0100

    add gradle wrapper and update kotlin and gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.konan.file.File
 import org.jetbrains.kotlin.konan.properties.loadProperties
 
 plugins {
-    kotlin("jvm") version "1.4.20" apply false
+    kotlin("jvm") version "1.4.21" apply false
     id("org.jetbrains.dokka") version "1.4.20" apply false
     id("com.gradle.plugin-publish") version "0.12.0" apply false
     id("com.github.gmazzo.buildconfig") version "2.0.2"
