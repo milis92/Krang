3af6cbdd8d67aa3796513a3d2c717f56e9365383
Ivan Milisavljevic <cartman.dev@gmail.com>
2021-01-02 04:12:18 +0100

    Fixes #4
    Gradle plugin now automatically applies runtime dependency

--- buildSrc/src/main/kotlin/publishing.kt
+++ buildSrc/src/main/kotlin/publishing.kt
@@ -19,21 +19,6 @@ import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.create
 
-/*
- * Copyright (C) 2020 Ivan Milisavljevic
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 const val github = "https://github.com/milis92/Krang"
 
 fun PublishingExtension.withDefaults(project: Project, desc: String) {
