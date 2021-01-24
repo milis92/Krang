36c187ef3d358469440c4ec2b3bce4a629d7f5e8
Ivan Milisavljevic <cartman.dev@gmail.com>
2021-01-24 01:27:08 +0100

    Fixes #11
    Added new Redact annotation used for removing parameters from Krang

--- sample/src/jvmMain/kotlin/com/herman/krang/sample/Main.kt
+++ sample/src/jvmMain/kotlin/com/herman/krang/sample/Main.kt
@@ -2,14 +2,15 @@ package com.herman.krang.sample
 
 import com.herman.krang.runtime.Krang
 import com.herman.krang.runtime.annotations.Intercept
+import com.herman.krang.runtime.annotations.Redact
 
 fun main() {
     Krang.addInterceptor { functionName, parameters ->
         println("Function with name:$functionName and ${parameters.joinToString()} called")
     }
-    testfunction("test", "df", "D")
+    testFunction("test", 1, true)
 }
 
 @Intercept
-fun testfunction(name: String, vararg test: Any?) {
+fun testFunction(@Redact a: String, @Redact b: Int, c: Boolean) {
 }
