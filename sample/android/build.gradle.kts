172666e14433d81df310a4271699aaa1e87f3925
Ivan Milisavljevic <cartman.dev@gmail.com>
2021-03-25 11:09:40 +0100

    Android sample (#15)
    Fixes #14
    
    Add working android sample

---
+++ sample/android/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("com.github.milis92.krang") version "2.0.2"
+}
+
+android {
+    compileSdkVersion(29)
+    defaultConfig {
+        applicationId = "com.herman.android"
+        minSdkVersion(15)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+    lintOptions {
+        isQuiet = true
+        isAbortOnError = false
+        isIgnoreWarnings = true
+        isCheckReleaseBuilds = false
+    }
+}
+
+krang {
+    enabled = true
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.2.0")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+        useIR = true
+    }
+}
