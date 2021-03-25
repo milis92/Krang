31b4a87de0ddb0fc53f9768ea6409c1e55504543
Ivan Milisavljevic <cartman.dev@gmail.com>
2021-03-25 01:57:37 +0100

    Update documentation

--- buildSrc/src/main/kotlin/android-project.gradle.kts
+++ buildSrc/src/main/kotlin/android-project.gradle.kts
@@ -6,8 +6,6 @@ android {
     compileSdkVersion(29)
     defaultConfig {
         minSdkVersion(15)
-        versionCode = 1
-        versionName = "1.0"
     }
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
