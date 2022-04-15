5e7e941fe254f33d9712e693f853a956f8e36d22
Ivan Milisavljevic <cartman.dev@gmail.com>
2022-04-15 02:23:32 +0200

    Unified compiler artefacts (#19)
    - Update Gradle to 7.4.1
    - Publish gradle scan on every task
    - Update to the new kotlin and kotlin compiler
    - Update to the new kotlin and kotlin compiler
    - Fix a bug with common platform not including runtime library
    - Update sample with new multiplatform library
    - Runtime is now compatible with all multiplatform targets (as common)
    - Unified native and regular compiler abi
    - Updated samples to reflect new changes

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -1,6 +1,25 @@
 rootProject.name = "sample"
 
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenLocal()
+        maven("https://oss.sonatype.org/content/repositories/snapshots")
+        mavenCentral()
+    }
+}
+
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
 include(":android")
 include(":common")
-
-/*includeBuild("..")*/
