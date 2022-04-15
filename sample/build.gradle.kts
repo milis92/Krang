681e0a514a1d474b0d6d3652da155d49c65fa5da
Ivan Milisavljevic <cartman.dev@gmail.com>
2022-04-15 02:18:14 +0200

    Fixes 18 - Runtime is now compatible with all multiplatform targets (as common) - Unified native and regular compiler abi - Updated samples to reflect new changes

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-    id("com.android.application") version "7.1.0-beta02" apply false
-    id("com.android.library") version "7.1.0-beta02" apply false
+    id("com.android.application") version "7.1.3" apply false
+    id("com.android.library") version "7.1.3" apply false
     id("org.jetbrains.kotlin.android") version "1.6.10" apply false
 }
 
 group = "com.herman.sample"
 version = "1.0"
+
+tasks.wrapper {
+    distributionType = Wrapper.DistributionType.ALL
+}
